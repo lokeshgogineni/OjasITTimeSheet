d73d53e8bf6a0b8afd3dc09b138515468e7ca82d
lokesh gogineni <lokeshgogineni@outlook.com>
2019-08-01 15:33:21 +0530

    01/08/2019 Commitall

--- OjasITTimeSheet/Controllers/SuperAdminController.cs
+++ OjasITTimeSheet/Controllers/SuperAdminController.cs
@@ -8,14 +8,51 @@ namespace OjasITTimeSheet.Controllers
 {
     public class SuperAdminController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        
 
         public IActionResult Dashboard()
         {
             return View();
         }
+
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAdmin()
+        {
+            return View();
+        }
+        public IActionResult AssignRoles()
+        {
+            return View();
+        }
+        public IActionResult AllRoles()
+        {
+            return View();
+        }
+        public IActionResult AddProject()
+        {
+            return View();
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+        public IActionResult ExportAllTimeSheets()
+        {
+            return View();
+        }
+        public IActionResult ExportAllExpense()
+        {
+            return View();
+        }
+        public IActionResult PushNotification()
+        {
+            return View();
+        }
+        
     }
 }
